00ae988aca72b06e89e154d78828c3f240d4b78b
anuragghsoh10 <anuragghosh1005@gmail.com>
2026-04-05 15:01:23 +0530

    Update database port configuration

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
  user: process.env.DB_USER,
  password: process.env.DB_PASSWORD,
  database: process.env.DB_NAME,
- port: process.env.DB_PORT || 25060,
+ port: process.env.DB_PORT,
  ssl: { rejectUnauthorized: false }
 });
 
